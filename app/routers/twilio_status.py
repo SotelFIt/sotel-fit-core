cf9718624cd75e8cfcd10d7487af2534f5892826
SotelFIt <sotellima@gmail.com>
2026-06-18 15:26:02 -0300

    debug(temp): adiciona presenca de TWILIO_TEMPLATE_PLANO ao diagnostico

--- app/routers/twilio_status.py
+++ app/routers/twilio_status.py
@@ -37,6 +37,8 @@ def twilio_status_debug():
         "public_backend_url_present": bool(os.getenv("PUBLIC_BACKEND_URL")),
         "public_backend_url_len": len(os.getenv("PUBLIC_BACKEND_URL", "")),
         "twilio_auth_token_present": bool(os.getenv("TWILIO_AUTH_TOKEN")),
+        "template_plano_present": bool(os.getenv("TWILIO_TEMPLATE_PLANO")),
+        "template_plano_len": len(os.getenv("TWILIO_TEMPLATE_PLANO", "")),
         "module_level_public_url_len": len(PUBLIC_BACKEND_URL),
         "module_level_token_present": bool(TWILIO_AUTH_TOKEN),
     }
